33539a8d88d1b196061c852e6d993b2b5336f474
sameed0909 <sameed1999.ahmed@gmail.com>
2024-09-27 16:45:17 +0500

    add styling

--- src/app/component/Cards.jsx
+++ src/app/component/Cards.jsx
@@ -19,32 +19,38 @@ const Cards = () => {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 p-10">
-      {ordersData.map((order, index) => (
-        <div
-          key={index}
-          className={`p-6 rounded-xl shadow-2xl transition-transform transform hover:scale-105 cursor-pointer ${
-            selectedCard === order.id ? 'bg-[#0A502C]' : 'bg-white'
-          }`}
-          onClick={() => handleCardClick(order)}
-        >
-          <h3 className={`text-xl font-bold mb-4 ${
-            selectedCard === order.id ? 'text-white' : 'text-black'
-          }`}>
-            {order.title}
-          </h3>
-          <p className={`text-5xl font-extrabold mb-2 ${
-            selectedCard === order.id ? 'text-white' : 'text-black'
-          }`}>
-            {order.count}
-          </p>
-          <p className={`text-sm ${
-            selectedCard === order.id ? 'text-white' : 'text-black'
-          }`}>
-            {order.description}
-          </p>
-        </div>
-      ))}
+    <div className="p-10">
+      {/* Overview Heading */}
+      <h2 className="text-3xl font-bold mb-6">Overview</h2>
+
+      {/* Cards Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 ">
+        {ordersData.map((order, index) => (
+          <div
+            key={index}
+            className={`p-6 rounded-xl transition-transform transform hover:scale-105 cursor-pointer border-2	${
+              selectedCard === order.id ? 'bg-[#0A502C]' : 'bg-white'
+            }`}
+            onClick={() => handleCardClick(order)}
+          >
+            <h3 className={`text-xl font-bold mb-4 ${
+              selectedCard === order.id ? 'text-white' : 'text-black'
+            }`}>
+              {order.title}
+            </h3>
+            <p className={`text-5xl font-extrabold mb-2 ${
+              selectedCard === order.id ? 'text-white' : 'text-black'
+            }`}>
+              {order.count}
+            </p>
+            <p className={`text-sm ${
+              selectedCard === order.id ? 'text-white' : 'text-black'
+            }`}>
+              {order.description}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
